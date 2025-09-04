ec0787dba2b72840f34edf7a24fd3eb10bbb05f7
Venkatsai-18 <venkatsai9725@gmail.com>
2025-09-04 15:53:18 +0530

    updated settings

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=&nfhg!+@!#r5r72hj*r8h9y(yo92$@=zsinh+wo^_7=b3fan("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
